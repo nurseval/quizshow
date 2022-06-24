2f2c6d2d53045df2217916c8e36263088b762f5b
Fatma Nur Seval <104559152+nurseval@users.noreply.github.com>
2022-06-24 16:39:58 +0300

    Add files via upload

---
+++ quizshow/Form2.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace quizshow
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnbasla = new System.Windows.Forms.Button();
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbldogru = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblsoru = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblzaman = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.textBox1.Font = new System.Drawing.Font("Mongolian Baiti", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(-1, -2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 189);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(202, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(12, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(202, 281);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnbasla
+            // 
+            this.btnbasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnbasla.Location = new System.Drawing.Point(819, -2);
+            this.btnbasla.Name = "btnbasla";
+            this.btnbasla.Size = new System.Drawing.Size(153, 32);
+            this.btnbasla.TabIndex = 5;
+            this.btnbasla.Text = "BAŞLA";
+            this.btnbasla.UseVisualStyleBackColor = true;
+            this.btnbasla.Click += new System.EventHandler(this.btnbasla_Click);
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblpuan.ForeColor = System.Drawing.Color.Transparent;
+            this.lblpuan.Location = new System.Drawing.Point(815, 70);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(72, 20);
+            this.lblpuan.TabIndex = 6;
+            this.lblpuan.Text = "PUAN : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(923, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // lbldogru
+            // 
+            this.lbldogru.AutoSize = true;
+            this.lbldogru.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldogru.ForeColor = System.Drawing.Color.Transparent;
+            this.lbldogru.Location = new System.Drawing.Point(829, 167);
+            this.lbldogru.Name = "lbldogru";
+            this.lbldogru.Size = new System.Drawing.Size(24, 20);
+            this.lbldogru.TabIndex = 6;
+            this.lbldogru.Text = " 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(829, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Soru :";
+            // 
+            // lblsoru
+            // 
+            this.lblsoru.AutoSize = true;
+            this.lblsoru.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsoru.ForeColor = System.Drawing.Color.Transparent;
+            this.lblsoru.Location = new System.Drawing.Point(923, 110);
+            this.lblsoru.Name = "lblsoru";
+            this.lblsoru.Size = new System.Drawing.Size(17, 18);
+            this.lblsoru.TabIndex = 6;
+            this.lblsoru.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(829, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Süre :";
+            // 
+            // lblzaman
+            // 
+            this.lblzaman.AutoSize = true;
+            this.lblzaman.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblzaman.ForeColor = System.Drawing.Color.Transparent;
+            this.lblzaman.Location = new System.Drawing.Point(923, 42);
+            this.lblzaman.Name = "lblzaman";
+            this.lblzaman.Size = new System.Drawing.Size(17, 18);
+            this.lblzaman.TabIndex = 6;
+            this.lblzaman.Text = "0";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(971, 487);
+            this.Controls.Add(this.lblzaman);
+            this.Controls.Add(this.lblsoru);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbldogru);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblpuan);
+            this.Controls.Add(this.btnbasla);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnbasla;
+        private System.Windows.Forms.Label lblpuan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbldogru;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblsoru;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblzaman;
+    }
+}
---
+++ quizshow/Form2.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace quizshow
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=.;Initial Catalog=sorular;Integrated Security=True");
+        int sayac = 0;
+        int puan = 0;
+        int zaman = 0;
+        private void btnbasla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            zaman = 21;
+         
+            button1.BackColor = Color.Gray;
+            button2.BackColor = Color.Gray;
+            button3.BackColor = Color.Gray;
+            button4.BackColor = Color.Gray;
+
+            btnbasla.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            btnbasla.Text = "Sonraki";
+
+            sayac++;
+
+            lblsoru.Text = sayac.ToString();
+
+            if (sayac == 1)
+            {
+
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *From soru1 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+
+                    textBox1.Text = (oku["soru"].ToString());
+                    lbldogru.Text = (oku["doğru"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 2)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru2 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    lbldogru.Text = (oku["doğru"].ToString());
+                }
+                baglan.Close();
+
+
+
+
+            }
+            if (sayac == 3)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru3 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    lbldogru.Text = (oku["doğru"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 4)
+            {
+                btnbasla.Text = "Oyun Bitti";
+                btnbasla.Enabled = false;
+               
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select *from soru4 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    lbldogru.Text = (oku["doğru"].ToString());
+                }
+                baglan.Close();
+            }
+            if(sayac == 5)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+          
+
+
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            lblsoru.Text = sayac.ToString();
+            button1.BackColor = Color.Gray;
+            button2.BackColor = Color.Gray;
+            button3.BackColor = Color.Gray;
+            button4.BackColor = Color.Gray;
+            lbldogru.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(button1.Text == lbldogru.Text)
+            {
+                puan = puan + 10;
+                lblpuan.Text = puan.ToString();
+                button1.BackColor = Color.Green;
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+
+            }
+            btnbasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(button2.Text == lbldogru.Text)
+            {
+                puan = puan + 10;
+                lblpuan.Text=puan.ToString();
+                button2.BackColor = Color.Green;
+
+            }
+            else
+            {
+                button2.BackColor = Color.Red;
+            }
+            btnbasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(button3.Text== lbldogru.Text)
+            {
+                puan =puan + 10;
+                lblpuan.Text = puan.ToString();
+                button3.BackColor = Color.Green;
+            }
+            else
+            {
+                button3.BackColor = Color.Red;
+            }
+            btnbasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(button4.Text== lbldogru.Text)
+            {
+                puan = puan + 10;
+                lblpuan.Text=puan.ToString();
+                button4.BackColor = Color.Green;
+            }
+            else
+            {
+                button4.BackColor= Color.Red;   
+            }
+            btnbasla.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman = zaman - 1;
+            lblzaman.Text = zaman.ToString();
+            if(zaman == 0)
+            {
+                timer1.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                btnbasla.Enabled = true;
+            }
+        }
+    }
+}
